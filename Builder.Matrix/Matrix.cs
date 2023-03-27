5bdec8a82328e4fbb38ef81aed94f5daf6ee3667
Minato <umi0n3@gmail.com>
2023-03-27 22:14:28 +0200

    add builder library

---
+++ Builder.Matrix/Matrix.cs
@@ -0,0 +1,70 @@
+﻿namespace Builder.Matrix;
+
+public class Matrix
+{
+    private const int From = 0;
+    private const int To = 99;
+
+    private int _trace;
+
+    public int[,] Array2D { get; }
+
+    public int Trace
+    {
+        get
+        {
+            for (var i = 0; i < Array2D.GetLength(0); i++)
+            {
+                _trace += Array2D[i, i];
+            }
+            return _trace;
+        }
+    }
+
+    public Matrix(int rowCount, int columnCount)
+    {
+        var random = new Random();
+        
+        Array2D = new int[rowCount, columnCount];
+
+        for (var i = 0; i < Array2D.GetLength(0); i++)
+        {
+            for (var j = 0; j < Array2D.GetLength(1); j++)
+            {
+                Array2D[i, j] = random.Next(From, To);
+            }
+        }
+    }
+
+    public void Print(bool isHighlightedDiagonal = true)
+    {
+        for (var i = 0; i < Array2D.GetLength(0); i++)
+        {
+            for (var j = 0; j < Array2D.GetLength(1); j++)
+            {
+                if (isHighlightedDiagonal && i == j)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                
+                if (Array2D[i, j] / 10 >= 1)
+                {
+                    Console.Write(Array2D[i, j] + " ");
+                }
+                else
+                {
+                    Console.Write(Array2D[i, j] + "  ");
+                }
+            }
+            Console.WriteLine();
+        }
+        
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+    
+    public void PrintLikeSnail(){}
+}
--- MatrixTrace/Program.cs
+++ MatrixTrace/Program.cs
@@ -1,9 +1,36 @@
-﻿namespace MatrixTrace;
+﻿using Builder.Matrix;
+
+namespace MatrixTrace;
 
 public static class Program
 {
     public static void Main(string[] args)
     {
+        int row;
+        int column;
+            
+        while (true)
+        {
+            try
+            {
+                Console.Write("Please, set row of matrix: ");
+                row = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Please, set column of matrix: ");
+                column = Convert.ToInt32(Console.ReadLine());
+                break;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please, write only numbers! Try again...");
+            }
+        }
         
+        var matrix = new Matrix(row, column);
+        
+        matrix.Print();
+
+        Console.WriteLine();
+        Console.Write("Trace (sum of main diagonal): ");
+        Console.WriteLine(matrix.Trace);
     }
 }
