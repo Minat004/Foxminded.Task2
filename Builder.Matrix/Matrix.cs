4bb8d54f62ea184c637c4273057c76a6cfa33901
Minato <umi0n3@gmail.com>
2023-03-29 12:47:45 +0200

    add initialize and remove print logic

--- Builder.Matrix/Matrix.cs
+++ Builder.Matrix/Matrix.cs
@@ -2,116 +2,76 @@
 
 public class Matrix
 {
-    private const int From = 0;
-    private const int To = 99;
+    public const int From = 0;
+    public const int To = 100;
 
     public Matrix(int rowCount, int columnCount)
     {
+        Array2D = new int[rowCount, columnCount];
+        ArraySnail = new int[Array2D.Length];
+
+        InitializeArray2D();
+        InitializeArraySnail();
+    }
+
+    public int[,] Array2D { get; }
+    
+    public int[] ArraySnail { get; }
+
+    public int Trace { get; private set; }
+
+    private void InitializeArray2D()
+    {
         var random = new Random();
         
-        Array2D = new int[rowCount, columnCount];
-
         for (var i = 0; i < Array2D.GetLength(0); i++)
         {
             for (var j = 0; j < Array2D.GetLength(1); j++)
             {
                 Array2D[i, j] = random.Next(From, To);
             }
-        }
-    }
 
-    public int[,] Array2D { get; }
-
-    private int _trace;
-    public int Trace
-    {
-        get
-        {
-            var shortLength = Array2D.GetLength(0) <= Array2D.GetLength(1)
-                ? Array2D.GetLength(0)
-                : Array2D.GetLength(1);
-            
-            for (var i = 0; i < shortLength; i++)
+            if (i < Array2D.GetLength(1))
             {
-                _trace += Array2D[i, i];
+                Trace += Array2D[i, i];
             }
-            
-            return _trace;
         }
     }
 
-    public void Print(bool isHighlightedDiagonal = true)
+    private void InitializeArraySnail()
     {
-        for (var i = 0; i < Array2D.GetLength(0); i++)
-        {
-            for (var j = 0; j < Array2D.GetLength(1); j++)
-            {
-                if (isHighlightedDiagonal && i == j)
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                }
-                else
-                {
-                    Console.ForegroundColor = ConsoleColor.White;
-                }
-                
-                if (Array2D[i, j] / 10 >= 1)
-                {
-                    Console.Write(Array2D[i, j] + " ");
-                }
-                else
-                {
-                    Console.Write(Array2D[i, j] + "  ");
-                }
-            }
-            Console.WriteLine();
-        }
-        
-        Console.ForegroundColor = ConsoleColor.White;
-    }
-
-    public void PrintSnailRange()
-    {
-        var snail = new int[Array2D.Length];
         var i = 0;
         var j = 0;
-        var wallI = new[] { 0, Array2D.GetLength(0) - 1 };
-        var wallJ = new[] { 0, Array2D.GetLength(1) - 1 };
+        var wallUp = 0;
+        var wallDown = Array2D.GetLength(0) - 1;
+        var wallLeft = 0;
+        var wallRight = Array2D.GetLength(1) - 1;
         
         for (var k = 0; k < Array2D.Length; k++)
         {
-            snail[k] = Array2D[i, j];
-            
-            if (k == Array2D.Length - 1)
-            {
-                Console.WriteLine(snail[k]);
-            }
-            else
-            {
-                Console.Write(snail[k] + ", ");
-            }
+            ArraySnail[k] = Array2D[i, j];
 
-            if (i == wallI[0] + 1 && j == wallJ[0])
+            if (i == wallUp + 1 && j == wallLeft && wallLeft != wallRight)
             {
-                wallI[0]++;
-                wallI[1]--;
-                wallJ[0]++;
-                wallJ[1]--;
+                wallUp++;
+                wallDown--;
+                wallLeft++;
+                wallRight--;
                 j = i;
             }
-            else if (i == wallI[0] && j != wallJ[1])
+            else if (i == wallUp && j != wallRight)
             {
                 j++;
             }
-            else if (j == wallJ[1] && i != wallI[1])
+            else if (j == wallRight && i != wallDown)
             {
                 i++;
             }
-            else if (i == wallI[1] && j != wallJ[0])
+            else if (i == wallDown && j != wallLeft)
             {
                 j--;
             }
-            else if (j == wallJ[0] && i != wallI[0])
+            else if (j == wallLeft && i != wallUp)
             {
                 i--;
             }
